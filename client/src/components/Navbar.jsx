6ec7ff54ba7ee637c8a315bdbf553eba44bfea85
EL-Magnific-o <khcom216@gmail.com>
2022-01-08 00:33:05 +0300

    ..

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () =>{
                 
             </div>
             <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
-                {['Market','Exchange','Tutorials','Wallets'].map((item, index)=>(
+                {['E-books','Audio books','Wallets','About us'].map((item, index)=>(
                   <NavbarItem key ={item + index} title={item} />     
                 ))}
                 <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
@@ -44,7 +44,7 @@ const Navbar = () =>{
                       <AiOutlineClose onClick={() => setToggleMenu(false)} />
 
                   </li>
-                  {['Market','Exchange','Tutorials','Wallets'].map((item, index)=>(
+                  {['E-books','Audio books','Wallets','About us'].map((item, index)=>(
                     <NavbarItem key ={item + index} title={item}classProps='my-2 text-lg' />     
                      ))}
               </ul>
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,12 +1,13 @@
-import React, {useContext, useLayoutEffect} from 'react';
+import React, { useContext } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
 import { TransactionContext } from "../context/TransactionContext";
-import { Loader } from "./";
+import { shortenAddress } from "../utils/shortenAddress";
+import { Loader } from ".";
 
-const CommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 const Input = ({ placeholder, name, type, value, handleChange }) => (
   <input
@@ -18,76 +19,73 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
     className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
   />
 );
-  
-const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const Welcome =() => {
-  const { connectWallet, currentAccount, formData, sendTransaction, handleChange } = useContext(TransactionContext); // chiek send
+const Welcome = () => {
+  const { currentAccount, connectWallet, handleChange, sendTransaction, formData, isLoading } = useContext(TransactionContext);
 
+  const handleSubmit = (e) => {
+    const { addressTo, amount, keyword, message } = formData;
 
-    const handleSubmit = (e) => {
-      const { addressTo, amount, keyword, message} = formData;
+    e.preventDefault();
 
-      e.preventDefault();
+    if (!addressTo || !amount || !keyword || !message) return;
 
-      if(!addressTo || !amount || !keyword || !message) return;
+    sendTransaction();
+  };
 
-      sendTransaction();
-
-    }
-    return (
-        <div className="flex w-full justify-center items-center">
-          <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-            <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
-              <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+  return (
+    <div className="flex w-full justify-center items-center">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Dive <br /> into World of Books
-              </h1>
-              <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+          </h1>
+          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
             Explore the the first Decentralized BookStore. Buy and sell Books Peer 2 Peer.
+          </p>
+          {!currentAccount && (
+            <button
+              type="button"
+              onClick={connectWallet}
+              className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+            >
+              <AiFillPlayCircle className="text-white mr-2" />
+              <p className="text-white text-base font-semibold">
+                Connect Wallet
               </p>
-              { !currentAccount && (
-               <button
-               type="button"
-               onClick={connectWallet}
-               className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-               >
-                 <p className="text-white text-base font-semibold">
-                    Connect Wallet
-                 </p>
-               </button>
-              )}
+            </button>
+          )}
 
-              <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
-                <div className={`rounded-tl-2xl ${commonStyles}`}>
-                  P2P
-                </div>
-                <div className={commonStyles}>Savings</div>
-                <div className={`sm:rounded-tr-2xl ${commonStyles}`}>
-                    Trust
-                </div>
-                <div className={`sm:rounded-bl-2xl ${commonStyles}`}>
-                   eBook
-                </div>
-                <div className={commonStyles}>Web 3.0</div>
-                <div className={`rounded-br-2xl ${commonStyles}`}>
-                   Audiobook 
-                </div>
-             </div>
+          <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+            <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
+               P2P
+            </div>
+            <div className={companyCommonStyles}>Savings</div>
+            <div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
+              Trust
+            </div>
+            <div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
+              eBook
+            </div>
+            <div className={companyCommonStyles}>Web 3.0</div>
+            <div className={`rounded-br-2xl ${companyCommonStyles}`}>
+              Audiobook
+            </div>
+          </div>
+        </div>
 
-            <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-              <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism">
-                <div className="flex justify-between flex-col w-full h-full">
-                  <div className="flex justify-between items-start">
-                    <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-                    <SiEthereum fontSize={21} color="#fff" />
-
-                    </div>
-                    <BsInfoCircle fontSize={17} color="#fff" />
-
-                  </div>
-                  <div>
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#fff" />
+                </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
+              </div>
+              <div>
                 <p className="text-white font-light text-sm">
-                  Address
+                  {shortenAddress(currentAccount)}
                 </p>
                 <p className="text-white font-semibold text-lg mt-1">
                   Ethereum
@@ -102,9 +100,10 @@ const Welcome =() => {
             <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
-            {false ? (
-              <Loader />
-              ) : (
+
+            {isLoading
+              ? <Loader />
+              : (
                 <button
                   type="button"
                   onClick={handleSubmit}
@@ -113,19 +112,11 @@ const Welcome =() => {
                   Send now
                 </button>
               )}
-            </div>
-          </div> 
-
-              
-
+          </div>
         </div>
-
       </div>
-
-        
     </div>
-
   );
-}
-  
-  export default Welcome;
+};
+
+export default Welcome;
